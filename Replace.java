ce18199e10cbe279b5e2d4c1b7aecd154d0038ea
Ashtalakshmi8 <ASHTALAKSHMI8>
2022-01-31 23:26:59 +0530

    weekly-test2

---
+++ Replace.java
@@ -0,0 +1,15 @@
+class Replac{
+        static String [] thing={"dog","fan","car"};
+          static void nau(){
+		      for(int index=0;index<thing.length;index++){
+			  System.out.print(thing[index]+"\t");
+	}
+		public static void main(String[]args){
+		System.out.println();
+                  nau();
+				  
+things[3]="dog";	
+nau();			  
+		}
+			  }
+}
